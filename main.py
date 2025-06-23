d187507dcfd73fbe59ca707c3aa030e00c3f8072
Vaibhav Singh <51132966+dextervaibhav@users.noreply.github.com>
2025-06-23 08:48:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 from fastapi import FastAPI, Request
-from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
+
 
 app = FastAPI()
-app.mount("/static", StaticFiles(directory="static"), name="static")
-templates = Jinja2Templates(directory="templates")
+
 
 @app.get("/")
-async def read_root(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+def read_root(request: Request):
+    return 'webiste is work in progress mode'
